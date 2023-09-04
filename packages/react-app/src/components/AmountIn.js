d7724cd07135f812db54eac418775cc86f99b2b3
daniel <danielmilewski123@gmail.com>
2023-09-04 13:25:01 +0200

    Components for tokens and balances created

---
+++ packages/react-app/src/components/AmountIn.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect, useRef } from 'react'
+
+import { chevronDown } from '../assets';
+import styles from '../styles';
+import { useOnClickOutside } from '../utils';
+
+function AmountIn() {
+    const [showList, setShowList] = useState(false);
+    return (
+        <div className={styles.amountContainer}>
+            <input 
+                placeholder='0.0'
+                type="number"
+                value=""
+                disabled="{false}"
+                onChange={() => {}}
+                className={styles.amountInput}
+            />
+
+            <div className="relative" onClick={() => setShowList((prevState) => !prevState)}>
+                <button className={styles.currencyButton}>
+                    {"ETH"}
+                    <img 
+                        src={chevronDown}
+                        alt="chevron down"
+                        className={`w-4 h4 object-contain ml-2 ${showList ? 'rotate-180': 'rotate-0'}`}
+                    />
+                </button>
+
+                {showList && (
+                    <ul className={styles.currencyList}>
+                        {[
+                            { token: 'ETH', tokenName: 'ETH' },
+                            { token: 'DAN Gold', tokenName: 'DGOLD' },
+                            { token: 'DAN Silver', tokenName: 'DSLV' }
+                        ].map(({token, tokenName}, index) => (
+                            <li key={index}
+                                className={`${styles.currencyListItem} ${true ? 'bg-site-dim2' : ''} cursor-pointer`}    
+                            >
+                                {tokenName}
+                            </li>
+                        ))}
+                    </ul>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default AmountIn
---
+++ packages/react-app/src/components/Balance.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { formatUnits, parseUnits } from 'ethers/lib/utils';
+
+import styles from '../styles';
+
+function Balance() {
+    const tokenBalance = parseUnits("0.00255")
+
+    return (
+        <div className={styles.balance}>
+            <p className={styles.balanceText}>
+                {tokenBalance && (
+                    <>
+                        <span className={styles.balanceBold}>Balance: </span>
+                        {formatUnits(tokenBalance || parseUnits("0"))}
+                    </>
+                )}
+            </p>
+        </div>
+    )
+}
+
+export default Balance
